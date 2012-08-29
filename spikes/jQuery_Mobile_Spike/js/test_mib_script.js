4e6427b09c1ef9280e288c6a6d294a04c971460e
Emily Casey <ecasey@ThoughtWorks.com>
2012-08-29 12:29:32 +0530

    [#14] (emily/king) began moving logic from java to js

--- spikes/jQuery_Mobile_Spike/js/test_mib_script.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with IntelliJ IDEA.
- * User: ThoughtWorks
- * Date: 8/28/12
- * Time: 11:10 AM
- * To change this template use File | Settings | File Templates.
- */
--- src/main/webapp/static/spec/mib_script.spec.js
+++ src/main/webapp/static/spec/mib_script.spec.js
@@ -1,7 +1,35 @@
-/**
- * Created with IntelliJ IDEA.
- * User: ThoughtWorks
- * Date: 8/28/12
- * Time: 11:28 AM
- * To change this template use File | Settings | File Templates.
- */
+
+
+describe("Idea", function() {
+
+    it("should not create an idea when passed an empty string", function(){
+
+        var emptyIdea= createIdea("");
+         expect(emptyIdea==null);
+
+    });
+
+    it("should not return null when passed a nonEmpty string", function(){
+
+        var newIdea= createIdea("hi hello");
+        expect(newIdea == null).toBe(false);
+
+    });
+
+    it("should get a valid json object containing message equal to message sent", function(){
+
+        var newIdea= createIdea("pineapple");
+        expect(newIdea.message).toEqual("pineapple");
+    });
+
+    xit("should should make POST request to 10.10.15.130:3000/points.json?point[section_id]=ID&point[message]=MESSAGE when passed valid MESSAGE", function(){
+        spyOn($, "ajax");
+        createIdea("pineapple");
+
+        expect($.ajax.mostRecentCall.args[0]["url"]).toEqual("10.10.15.130:3000/points.json?point[section_id]=2&point[message]=pineapple");
+    });
+
+
+
+
+});
