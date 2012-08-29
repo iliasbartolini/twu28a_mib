d0f3cd1e1bc88da4c963084a7e63f726e22f23d9
Mai K Lam <mai.lam@lamkhanhmai.com>
2012-08-29 13:54:41 +0530

    Mai: add event to view

--- spikes/BackboneJS_Spike/js/mib.js
+++ spikes/BackboneJS_Spike/js/mib.js
@@ -1,68 +1,12 @@
 //var cache = new CacheProvider;
 
-/**
- * IndexView: The default view seen when opening up the application for the first time.
- * This will be just a message to request user to enter specific board URL for now
- * @type Backbone.View
- */
-var IndexView = Backbone.View.extend({
-    el: $('#container'),
-    template: _.template($("#template-index").html()),
+$(function() {
+    Backbone.emulateJSON = true;
 
-    initialize: function() {
-        this.render();
-    },
+    // Initialize Backbone views.
+    var appRouter = new AppRouter();
 
-    render: function() {
-        var html = this.template();
-        $(this.el).html(html);  // Append the result to the view's element.
-        return this;
-    }
+    // Initialize the Backbone router.
+    Backbone.history.start();
+
 });
-
-
-/**
- * CreateIdeaView: The view for creating new idea
- * For now will be the default page when enter a correct Board URL
- * @type Backbone.View
- */
-var CreateIdeaView = Backbone.View.extend({
-    el: $('#container'),
-    template: _.template($("#template-newIdea").html()),
-    _boardName: null,
-    _boardID: null,
-
-    initialize: function(boardName, id) {
-        this._boardName = boardName;
-        this._boardID = id;
-        this.render();
-    },
-
-    render: function(){
-        var html = this.template({ boardName: this._boardName, boardId: this._boardID });
-        $(this.el).html(html);  // Append the result to the view's element.
-        return this;
-    }
-});
-
-
-
-
-var AppRouter = Backbone.Router.extend({
-    routes: {
-        "": "index", // http://localhost/
-        "for/:boardName/:id": "createIdea" // #for/mibTest/9
-    },
-    index: function(){
-        var indexView = new IndexView();
-    },
-    createIdea: function(boardName, id){
-        var createIdeaView = new CreateIdeaView(boardName, id);
-    }
-});
-
-// Initiate the router
-var app_router = new AppRouter;
-
-// Start Backbone history a neccesary step for bookmarkable URL's
-Backbone.history.start();
