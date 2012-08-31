69c832d07570718fae5bcc3d84643d3cbc87c615
nagarajr <nagarajr@thoughtworks.com>
2012-08-31 17:53:00 +0530

    Revert "Nagaraj/Anoop:API's for Comment Partially Implemented"
    This reverts commit dcfc3e78503d6b1560ccfad52805311c90f33dda.

--- src/main/java/com/thoughtworks/twu/domain/Comment.java
+++ src/main/java/com/thoughtworks/twu/domain/Comment.java
@@ -1,42 +1,20 @@
 package com.thoughtworks.twu.domain;
 
-import java.util.Date;
-
-
+/**
+ * Created with IntelliJ IDEA.
+ * User: nagarajr
+ * Date: 30/8/12
+ * Time: 1:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
 public class Comment  {
     public String name;
     public String comment;
     public int id;
-    private Date createdAt;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Comment comment1 = (Comment) o;
-
-        if (id != comment1.id) return false;
-        if (comment != null ? !comment.equals(comment1.comment) : comment1.comment != null) return false;
-        if (name != null ? !name.equals(comment1.name) : comment1.name != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (comment != null ? comment.hashCode() : 0);
-        result = 31 * result + id;
-        return result;
-    }
 
     public Comment(String name, String comment) {
 
         this.name = name;
         this.comment = comment;
-        this.createdAt = new Date();
     }
-
-
 }
--- src/main/java/com/thoughtworks/twu/service/CommentService.java
+++ src/main/java/com/thoughtworks/twu/service/CommentService.java
@@ -1,19 +1,9 @@
 package com.thoughtworks.twu.service;
 import com.thoughtworks.twu.domain.Comment;
-import com.thoughtworks.twu.persistence.ICommentMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class CommentService implements ICommentService{
-    ICommentMapper commentMapper ;
-
-    @Autowired
-    public CommentService(ICommentMapper commentMapper) {
-        this.commentMapper = commentMapper;
-    }
     @Override
-
     public Comment addNewComment(String name, String comment) {
-        Comment savedComment = commentMapper.insertComment(new Comment(name,comment));
-        return savedComment;
+        return new Comment(name,comment);
     }
 }
