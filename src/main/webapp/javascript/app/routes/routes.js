9dd28fdd44c28949ad5737650de749ec3f46a7f0
Magdalena Cervantes Cassel <magdalena.cassel@gmail.com>
2012-08-31 12:18:29 +0530

    [](magdalena/vishnu) fixed paths to sources in index.html

--- src/main/webapp/javascript/app/routes/routes.js
+++ src/main/webapp/javascript/app/routes/routes.js
@@ -7,7 +7,6 @@ var AppRouter = Backbone.Router.extend({
         var indexView = new IndexView($('#container'),$("#template-index"));
     },
     createIdea: function(boardName, id){
-        console.log("oh no pigeons");
         var createIdeaView = new CreateIdeaView($("#container"), $("#template-newIdea"), $("submitBtn"), $("ideaText"), boardName, id);
     }
 });
