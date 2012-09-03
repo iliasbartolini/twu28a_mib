7a8f6d38162fb858a7b1e94e7403bd402724ad5a
vishnu <vishnu@vishnu-Latitude-E6420.(none)>
2012-09-03 13:15:26 +0530

    Revert "[#14](vishnu/magdalena) Working with createIdeaView"
    This reverts commit 8ea6929172f053e031556b82ffcd330f5c5578b5.

--- src/main/webapp/javascript/app/views/create_idea_view.js
+++ src/main/webapp/javascript/app/views/create_idea_view.js
@@ -1,36 +1,43 @@
+
 var CreateIdeaView = Backbone.View.extend({
-    el: $('#container'),
-    template: _.template($("#template-newIdea").html()),
+    el: null,
+    template: null,
+    _ideaText : null,
     _boardName: null,
     _boardID: null,
 
-    initialize: function(boardName, id) {
+    events: {
+        "click button#submitBtn": "submitIdea"
+    } ,
 
+    initialize: function(container, template, ideaText, boardName, id) {
+        console.log("in initialize");
+        this.el = container;
+        this.template = _.template(template);
         this._boardName = boardName;
         this._boardID = id;
+        this._ideaText = ideaText;
+
+        //IdeaBoardz.WebIdeaBoardz.instance.getSections();
+
         this.render();
     },
 
     render: function(){
         var html = this.template({ boardName: this._boardName, boardId: this._boardID });
         $(this.el).html(html);  // Append the result to the view's element.
+        this.delegateEvents();
         return this;
     },
 
-    events: {
-        "click #submitBtn": "submitIdea"
-    },
-
     submitIdea: function(event){
+
         console.log("in submitIdea before call to createIdea");
-        var message = $(this.el).find("#ideaText").val();
-        console.log(message);
+        var result = IdeaBoardz.WebIdeaBoardz.instance.createIdea(this._ideaText.val());
 
-        IdeaBoardz.WebIdeaBoardz.instance.createIdea(message);
-        //this.el.find("#ideaText").val("");
-
+        // tell, don't ask
+        // delegation
         return false;
     }
-
 
 });
