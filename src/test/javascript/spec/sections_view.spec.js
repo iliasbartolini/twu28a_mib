978c72b55490fd12b80d847d2feee5b4b30c3eec
Emily Casey <ecasey@ThoughtWorks.com>
2012-09-04 14:43:49 +0530

    [#9] (mai/emily) changed homepage to sectionsView and tested routes. Began getting section names.

---
+++ src/test/javascript/spec/sections_view.spec.js
@@ -0,0 +1,11 @@
+describe("Section Page View", function(){
+
+   it("should retrieve board name when new SectionsView is created", function(){
+
+       var view= new SectionsView("mibtest", "9");
+
+
+
+   });
+
+});
