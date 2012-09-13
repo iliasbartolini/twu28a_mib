e863cf07359d018b3246d1d51537b271b4ad3195
vishnun <vishnun@thoughtworks.com>
2012-09-13 12:15:42 +0530

    [tech](vishnu/Nagaraj) Fixing onEnter bug for comment post

--- src/main/webapp/javascript/app/views/create_comment_view.js
+++ src/main/webapp/javascript/app/views/create_comment_view.js
@@ -9,10 +9,14 @@ $(document).ready(function() {
         timer: 0,
 
         events: {
+            "onkeydown": "submitOnEnter",
             "click #postBtn": "postAComment",
             "click #commentBtn": "render"
         },
 
+        submitOnEnter: function(event){
+            console.log(event.key);
+        },
         initialize: function(container, boardName, id) {
             console.log("in initialize Create Comments View");
             this._boardID = id;
